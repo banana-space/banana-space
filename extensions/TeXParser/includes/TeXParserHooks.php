e9c257fd4703ed01959967e80b78a154ac44dee2
alphaGem <leono0.618@gmail.com>
2020-12-02 21:57:07 +0800

    Raifort Update: Added more demo.

--- extensions/TeXParser/includes/TeXParserHooks.php
+++ extensions/TeXParser/includes/TeXParserHooks.php
@@ -7,28 +7,35 @@
  */
 
 class TeXParserHooks {
+	static function http_post_json($url, $jsonStr){
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_POST, 1);
+	    curl_setopt($ch, CURLOPT_URL, $url);
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	            'Content-Type: application/json; charset=utf-8',
+	            'Content-Length: ' . strlen($jsonStr)
+	        )
+	    );
+	    $response = curl_exec($ch);
+	    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	    curl_close($ch);
 
-	public static function parserFunctionEcho( $parser, $value ) {
-		return '<pre>Echo Function: ' . htmlspecialchars( $value ) . '</pre>';
-	}
-
-
-	public static function onParserFirstCallInit( &$parser ) {
-		// Add the following to a wiki page to see how it works:
-		// {{#echo: hello }}
-		$parser->setFunctionHook( 'echo', 'TeXParserHooks::parserFunctionEcho' );
-
-		return true;
+	    return array($httpCode, $response);
 	}
 
 	public static function onParserBeforeInternalParse( &$parser, &$text, &$stripState ){
 		if(!$parser->getOptions()->getInterfaceMessage())
 		{
-			$text='Bon!';
+			//Content detected, should send it to the parser js exec.
+
+			$url="http://127.0.0.1:7200";
+			$jsonStr=json_encode(array("code" => $text));
+			list($returnCode, $returnContent) = self::http_post_json($url, $jsonStr);
+			$text = json_decode($returnContent)->html;
 			return false;
 		}
 		return true;
 	}
-
-
 }
