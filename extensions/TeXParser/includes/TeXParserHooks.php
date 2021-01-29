fb38d12d6951620253496de5c2bbae58eb460de6
abccsss <buchenjing@outlook.com>
2021-01-30 00:50:27 +0800

    Handle labels

--- extensions/TeXParser/includes/TeXParserHooks.php
+++ extensions/TeXParser/includes/TeXParserHooks.php
@@ -7,7 +7,7 @@
  */
 
 class TeXParserHooks {
-	static function http_post_json($url, $jsonStr){
+	static function http_post_json($url, $jsonStr) {
 	    $ch = curl_init();
 	    curl_setopt($ch, CURLOPT_POST, 1);
 	    curl_setopt($ch, CURLOPT_URL, $url);
@@ -25,20 +25,42 @@ class TeXParserHooks {
 	    return array($httpCode, $response);
 	}
 
-	public static function onParserBeforeInternalParse( &$parser, &$text, &$stripState ){
-		if(!$parser->getOptions()->getInterfaceMessage())
-		{
-			//Content detected, should send it to the parser js exec.
+	public static function onParserBeforeInternalParse( Parser &$parser, &$text, &$stripState ) {
+		// If text is page content, use bTeX.
+		if (!$parser->getOptions()->getInterfaceMessage()) {
+			// Read labels from database
+			$title = $parser->getTitle();
+			$labels = LabelHandler::getLabels($title);
 
-			$url="http://127.0.0.1:7200";
-			$jsonStr=json_encode(array("code" => $text));
-			list($returnCode, $returnContent) = self::http_post_json($url, $jsonStr);
-			$text = json_decode($returnContent)->html;
+			// Run compiler
+			$url = "http://127.0.0.1:7200";
+			$jsonStr = json_encode(
+				array(
+					"code" => $text,
+					"labels" => $labels
+				)
+			);
+			list($httpCode, $response) = self::http_post_json($url, $jsonStr);
+			$json = json_decode($response);
+
+			// Output
+			$text = $json->html;
+
+			// When not in preview mode,
+			// write labels to database
+			if ($parser->getRevisionId() !== null) {
+				LabelHandler::setLabels($title, $json->labels);
+			}
+
 			return false;
 		}
+
+		// Text is not page content; use wikitext parser.
 		return true;
 	}
-	public static function onBeforePageDisplay( OutputPage $output, Skin $skin ){
+
+	public static function onBeforePageDisplay( OutputPage $output, Skin $skin ) {
+		// Load css for btex output
 		$output->addModules( "ext.TeXParser" );
 	}
 }
--- extensions/TeXParser/includes/TeXParserMagic.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$magicWords = [];
-
-$magicWords['zh-hans'] = [
-	'echo' => [ 0, 'echo' ],
-];
